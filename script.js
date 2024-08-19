dc68ab5f413800446584d1d2d4292e7ef6e33d76
Luu-B <142186981+Luu-B@users.noreply.github.com>
2024-08-19 23:07:56 +0000

    Version 3 - Un-styled nav - working bg and banner

--- script.js
+++
@@ -1,8 +0,0 @@
-$('.navbar').affix({offset: {top: 20} });
-
-// scroll blur
-$(window).on('scroll', function () {
-    var pixs = $(document).scrollTop()
-    pixs = pixs / 100;
-    $(".bg-image").css({"-webkit-filter": "blur("+pixs+"px)","filter": "blur("+pixs+"px)" });
-});
